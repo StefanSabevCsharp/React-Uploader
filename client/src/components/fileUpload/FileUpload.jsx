2396469094c4e75b47a686d186da909d02d01521
Stefan Sabev <stevenbg91@gmail.com>
2024-08-15 09:44:49 +0200

    Add new component FileUpload

---
+++ client/src/components/fileUpload/FileUpload.jsx
@@ -0,0 +1,7 @@
+export default function FileUpload() {
+    return (
+        <>
+          <h1 className="text-center" ><i className="fa-brands fa-react"></i> React File Upload</h1>
+        </>
+    );
+}
