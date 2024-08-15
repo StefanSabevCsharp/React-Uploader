756220d63ab1d646b089b05e79d274e8df1be2b4
Stefan Sabev <stevenbg91@gmail.com>
2024-08-15 13:42:59 +0200

    Add file component

---
+++ client/src/components/file/File.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+export default function File({ file }) {
+ 
+    const uploadFilePath = `${file.filePath}`;
+    console.log(uploadFilePath);
+   
+
+    return (
+        <div className="row mt-5">
+            <div className="col-md-6 m-auto">
+                <h3 className="text-center">{file.fileName}</h3>
+                <img
+                    src={file.filePath}
+                    style={{ width: "100%", height: "auto", marginTop: "30px" , marginBottom: "30px"}}
+                />
+            </div>
+        </div>
+    );
+}
