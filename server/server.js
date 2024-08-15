9cf0f5cfdd81002f20b079c008e20084c8fabd63
Stefan Sabev <stevenbg91@gmail.com>
2024-08-15 09:43:12 +0200

    Changing file paths using path

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 
 const app = express();
 app.use(fileUpload());
+const uploadPath = path.join(__dirname, '..', 'client', 'public', 'uploads');
 
 app.post("/upload", (req,res) => {
     if(req.files === null){
         return res.status(400).json({msg: "No file uploaded"});
     }
     const file = req.files.file;
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`,err => {
+    const filePath = path.join(uploadPath, file.name);
+
+    file.mv(filePath,err => { 
         if(err){
             console.error(err);
             return res.status(500).send(err);
