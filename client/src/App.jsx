b1727d6dfb565ee08515545aa9f8ebe58718d9c2
Stefan Sabev <stevenbg91@gmail.com>
2024-08-15 09:45:05 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import FileUpload from "./components/fileUpload/FileUpload"
+import Upload from "./components/upload/Upload"
 
 
 function App() {
- 
 
   return (
-    <>
-    </>
+    <div className="container d-flex justify-content-center mt-4">
+      <div className="w-100" style={{maxWidth: "800"}}>
+        <FileUpload />
+        <Upload />
+      </div>
+    </div>
+
   )
 }
 
