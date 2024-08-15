118b29f05dbd579de96d44ab0e64add1c4192e8e
Stefan Sabev <stevenbg91@gmail.com>
2024-08-15 07:59:28 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.post("/upload", (req,res) => {
+    
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+})
